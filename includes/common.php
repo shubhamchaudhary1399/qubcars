b3fc0efc500f52cca0ae456facebcbff61672933
Shubham Chaudhary <shubhamchaudhary1399@gmail.com>
2021-10-28 13:24:46 +0530

    changing db configuration

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "qubcars")
+    $con = mysqli_connect("us-cdbr-east-04.cleardb.com", "bb1526a897a02e", "8601874e", "heroku_79bc2955dfae004")
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();
